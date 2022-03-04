1fa435a67329f406f518f93b034aa5d8c300c52c
Adam Haley <adamhaley@Adams-Computer.local>
2022-03-03 20:14:11 -0800

    adding a task in constructor, added swap files to gitignore

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -5,6 +5,21 @@ contract TodoList {
     uint taskCount = 0;
 
     struct Task {
-
+        uint id;
+        string content;
+        bool completed;
     }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
