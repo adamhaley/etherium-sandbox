c72c0cb48bd72f4213369e20bde82f93aeefe3d2
Adam Haley <adamhaley@Adams-Computer.local>
2022-03-05 17:34:23 -0800

    rendering tasks.. (not fully working yet)

--- src/app.js
+++ src/app.js
@@ -59,10 +59,39 @@ let App = {
             return;
         }
 
+        App.setLoading(true);
+
+        $('#account').html(App.account)
+
+        await App.renderTasks();
+
         App.setLoading(false);
     },
     renderTasks: async() => {
+        const taskCount = await App.todoList.taskCount();
+        const $taskTemplate = $('.taskTemplate');
 
+        for(var i=1;i <= taskCount; i++){
+            const task = await App.todoList.tasks(i);
+            const taskId = task[0].toNumber();
+            const taskContent = task[1];
+            const taskCompleted = task[2];
+
+            const $newTaskTemplate = $taskTemplate.clone();
+            $newTaskTemplate.find('.content').html(taskContent);
+            $newTaskTemplate.find('input')
+                        .prop('name', taskId)
+                        .prop('checked', taskCompleted)
+    //                    .on('click', App.toggleCompleted);
+      
+            if(taskCompleted){
+                $('completedTaskList').append($newTaskTemplate);
+            }else{
+                $('#taskList').append($newTaskTemplate);
+            }
+            
+            $newTaskTemplate.show();
+        }
     },
     setLoading: (boolean) => {
         App.loading = boolean;
